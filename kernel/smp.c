8eee7c5d61ecb305a1dc1285ac4d3cfe6f8f200a
franciscofranco <franciscofranco.1990@gmail.com>
2014-10-04 18:39:48 +0000

    Squash 'enhance power efficiency' patches
    Series of patches from NVIDIA on some critical blocking
    code paths to allow the CPU to relax instead of busy spinning
    thus lowering the load_average and lowering power consumption.
    
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	kernel/sched/core.c
    	kernel/smp.c

--- kernel/smp.c
+++ kernel/smp.c
@@ -12,6 +12,7 @@
 #include <linux/gfp.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <asm/relaxed.h>
 #include <linux/of.h>
 
 #include "smpboot.h"
@@ -129,8 +130,8 @@ void __init call_function_init(void)
 static void csd_lock_wait(struct call_single_data *csd)
 {
 	set_csd_lock_waiting_flag();
-	while (csd->flags & CSD_FLAG_LOCK)
-		cpu_relax();
+	while (cpu_relaxed_read_short(&csd->flags) & CSD_FLAG_LOCK)
+		cpu_read_relax();
 	clear_csd_lock_waiting_flag();
 }
 
