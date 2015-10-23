01daa9634c6a404a6cfb99c80a9c99942667628c
Anoop Kumar <anoopkumar1119@gmail.com>
2015-10-23 16:43:52 +0530

    Reduced_VFS_cache_pressure

--- build_kernel.sh
+++ build_kernel.sh
@@ -41,7 +41,7 @@ export KBUILD_BUILD_USER="anoopkumar"
 export KBUILD_BUILD_HOST="helllfire"
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/anoopkumar/android/toolchains/arm-linux-eabi-UB-4.9/bin/arm-eabi-
+export CROSS_COMPILE=/home/anoopkumar/android/toolchains/arm-cortex_a53_linaro-linux-gnueabi/bin/arm-cortex_a53_linaro-linux-gnueabi-
 
 compile_kernel ()
 {
