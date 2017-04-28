da1c0772edb7a67c12671dd040b2bad59da97f39
Anoop Kumar <anoopkumar1119@gmail.com>
2017-04-28 12:25:59 +0530

    Phoenix setup

---
+++ cwm_flash_zip/tools/flash_kernel.sh
@@ -0,0 +1,19 @@
+#!/sbin/sh
+#
+# Live ramdisk patching script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+
+echo \#!/sbin/sh > /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x$(cat /tmp/boot.img-base) --pagesize $(cat /tmp/boot.img-pagesize) --ramdisk_offset 0x$(cat /tmp/boot.img-ramdiskoff) --tags_offset 0x$(cat /tmp/boot.img-tagsoff) --dt /tmp/dt.img --output /tmp/newboot.img >> /tmp/createnewboot.sh
+
